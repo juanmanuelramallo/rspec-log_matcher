64339939149a686023abf6acd19dac1de8c18eb3
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2020-09-19 21:43:58 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in rspec-log_matcher.gemspec
+gemspec
---
+++ lib/rspec/log_matcher.rb
@@ -0,0 +1,10 @@
+require 'rspec/log_matcher/version'
+require 'rspec/log_matcher/matcher'
+require 'rspec/log_matcher/config'
+
+module Rspec
+  module LogMatcher
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
