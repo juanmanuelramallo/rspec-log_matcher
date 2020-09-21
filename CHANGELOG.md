5d8770a2e7e0bd6eccfac99e9ed00c9f1da1bfda
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2020-09-21 14:02:49 -0300

    Release 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # master
 
+# 1.1.0
+
 * Adding a class method `configure!` to configure the matcher
+
+    *Juan Manuel Ramallo*
 
 # 1.0.0
 
--- lib/rspec/log_matcher/version.rb
+++ lib/rspec/log_matcher/version.rb
@@ -3,7 +3,7 @@
 module Rspec
   module LogMatcher
     MAJOR = 1
-    MINOR = 0
+    MINOR = 1
     PATCH = 0
 
     VERSION = [MAJOR, MINOR, PATCH].join('.')
